69fc9f17d8510c5a6c43f1f797fb6281bce09c47
Your Name <rajarajan115>
2021-07-28 21:50:02 +0530

    Added Firebase Dependency

--- app/src/main/java/com/raja/internetspeed/MainActivity.java
+++ app/src/main/java/com/raja/internetspeed/MainActivity.java
@@ -2,13 +2,29 @@ package com.raja.internetspeed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.raja.internetspeed.Support.Connectivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "SpeedDetails";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "getNetworkInfo_" + Connectivity.getNetworkInfo(getApplicationContext()));
+        Log.d(TAG, "isConnected_" + Connectivity.isConnected(getApplicationContext()));
+        Log.d(TAG, "isConnectedWifi_" + Connectivity.isConnectedWifi(getApplicationContext()));
+        Log.d(TAG, "isConnectedMobile_" + Connectivity.isConnectedMobile(getApplicationContext()));
+//        Log.d(TAG, "" + Connectivity.isConnectionFast(getApplicationContext(), ConnectivityManager.TYPE_MOBILE));
+
+//        665
+//                576,577
+
     }
 }
